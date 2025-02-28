85542a55669bbdf7e67130bf4bb1f0047ad41d8b
Nánási <nanasizoltan20010629@gmail.com>
2025-02-28 16:21:21 +0100

    Beépítésre került egy automatikus kitöltés script a CV készítő oldalra, ami irányítószám alapján kitölti automatikusan a Megye és Város mezőt

---
+++ .history/resources/js/app_20250228122621.js
@@ -0,0 +1,57 @@
+import '../css/app.css';
+import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import '@mdi/font/css/materialdesignicons.css';
+import { createPinia } from 'pinia';
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const pinia = createPinia();
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons:{
+        defaultSet:'mdi',
+        aliases:{
+            sets:{
+                mdi,
+            },
+        },
+    },
+    theme:{
+        defaultTheme: 'light',
+    },
+    ssr:true,
+});
+
+createInertiaApp({
+    title: (title) => `${title}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(vuetify)
+            .use(pinia)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});

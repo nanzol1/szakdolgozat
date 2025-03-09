b2d0f568b9df3daa250eea7a5689a8dae28abb68
nanzol1 <nanasizoltan20010629@gmail.com>
2025-03-09 20:10:07 +0100

    Delete .history directory

--- .history/app/Http/Controllers/CVController_20250216163147.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers;
-
-class CVController extends Controller{
-    
-}
-
-?>
--- .history/resources/js/Pages/Profile/UserProfile/Cv/Templates/CV_Template1_20250226181720.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import InputLabel from '@/Components/InputLabel.vue';
-</script>
-
-<template>
-  <div>
-    Hello1
-  </div>
-</template>
